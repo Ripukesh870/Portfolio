9dc37ac7520adce1991db2c6089f179863b6102e
Ripukesh Kumar <ripukesh870@gmail.com>
2025-04-29 17:27:42 +0530

    image or files added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,52 @@
-
-
-
-
+// import { useState } from 'react';
 import { motion } from "framer-motion";
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import "../css/Project.css";
 import img1 from "../assets/img1.jpg";
+import portfolioimg from "../assets/portfolioimg.png";
+import musicimg from "../assets/musicimg.png";
+import youtubeimg from "../assets/youtubeimg.png";
+import textutilsimg from "../assets/textutilsimg.png"
 
 const projects = [
   {
     title: "Portfolio Website",
-    description: "A responsive personal portfolio built using React, Tailwind CSS, and Framer Motion.",
-    tools: ["React", "Tailwind CSS", "Framer Motion"],
-    image: img1,
-    github: "#",
-    demo: "#",
+    // description: "A responsive personal portfolio built using React, Tailwind CSS, and Framer Motion.",
+    description: "Built a responsive portfolio website using React.js, styled with Material UI and Bootstrap. Added animations with Framer Motion and particle effects using tsParticles. Used Typewriter Effect and React Icons for visual elements, and ensured code quality with Testing Library and Web Vitals.",
+    tools: ["React","MUI","Frame-Motion","React Icon","Bootstrap", "Typewriter Effect"],
+    image: portfolioimg,
+    github: "https://github.com/Ripukesh870/Portfolio",
+    demo: "https://ripukeshportfolio.netlify.app/",
   },
   {
-    title: "Image Compressor App",
-    description: "Compress images easily using React and browser APIs.",
-    tools: ["React", "Compressor.js", "Bootstrap"],
-    image: img1,
-    github: "#",
-    demo: "#",
+    title: "music web app",
+    description: "Developed a music web app using React.js with Material UI (MUI) for UI components and layouts. Integrated Redux Toolkit for state management and React Router for efficient navigation. Enhanced performance with Web Vitals and testing using React Testing Library. Employed modern JavaScript libraries and tools to create a smooth and interactive user experience.",
+    tools: ["React","MUI","redux toolkit","react-router-dom", "react-redux"],
+    image: musicimg,
+    github: "https://github.com/Ripukesh870/RipuMusic",
+    demo: "https://ripumusic.netlify.app/",
   },
   {
-    title: "Image Compressor App",
-    description: "Compress images easily using React and browser APIs.",
-    tools: ["React", "Compressor.js", "Bootstrap"],
-    image: img1,
-    github: "#",
-    demo: "#",
+    title: "youtube clone",
+    description: "Built a YouTube clone using React.js, Redux, and React Router for efficient state management and routing. Leveraged Material UI (MUI) for responsive design and Tailwind CSS for utility-first styling. Implemented Redux Toolkit for simplified state management and used Emotion.js for custom styling. Integrated React Icons for scalable icons, and Tailwind Scrollbar for customized scrollbars.",
+    tools: ["React", "redux", "react icon", "Tailwind CSS","Emotion.js","Tailwind Scrollbar" ,"MUI"],
+    image: youtubeimg,
+    github: "https://github.com/Ripukesh870/Youtube_Clone",
+    demo: "https://ripu-youtube-clone.netlify.app/",
   },
   {
-    title: "Image Compressor App",
+    title: "TextUtils",
     description: "Compress images easily using React and browser APIs.",
     tools: ["React", "Compressor.js", "Bootstrap"],
-    image: img1,
-    github: "#",
-    demo: "#",
+    image: textutilsimg,
+    github: "https://github.com/Ripukesh870/textUtils",
+    demo: "https://textutils1219.netlify.app/",
   },
-  {
-    title: "Image Compressor App",
-    description: "Compress images easily using React and browser APIs.",
-    tools: ["React", "Compressor.js", "Bootstrap"],
-    image: img1,
-    github: "#",
-    demo: "#",
-  },
+  
 ];
 
 const Projects = () => {
+
   return (
     <section id="projects" className="min-vh-100 p-5 bg-dark text-light">
       <motion.h2
@@ -97,7 +93,11 @@ const Projects = () => {
                   <h3 className="card-title h5 fw-bold text-light">{project.title}</h3>
 
                   {/* Description */}
-                  <p className="card-text color-black flex-grow-1 mt-0">{project.description}</p>
+
+                  <p className="card-text color-black flex-grow-1 mt-0 truncate-2-lines" style={{fontFamily: 'Arial, sans-serif'}}> {project.description} </p>
+
+                
+
 
                   {/* Tools Used */}
                   <div className="mb-2">
